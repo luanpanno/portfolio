c041c6ac670c9a68331d342b62f64e8f0bdd99e5
Luan Panno <luanpanno@gmail.com>
2021-01-20 23:39:40 -0300

    style: copyright space

--- src/containers/layout/Footer/index.tsx
+++ src/containers/layout/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Container } from './styles';
 const Footer = () => {
   return (
     <Container>
-      Desenvolvido por Luan Panno - Todos os direitos reservados &copy;
+      Desenvolvido por Luan Panno - Todos os direitos reservados &copy;{' '}
       {new Date().getFullYear()}
     </Container>
   );
