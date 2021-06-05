150cf6d8f9ccebfa212e4756dcff6f2ff92137e1
Luan Panno <luanpanno@gmail.com>
2021-06-05 00:08:17 -0300

    feat: formsubmit

--- src/pages/Home/components/Contact/Form/index.tsx
+++ src/pages/Home/components/Contact/Form/index.tsx
@@ -2,13 +2,13 @@ import { Form } from './styles';
 
 export const ContactForm = () => {
   return (
-    <Form>
+    <Form action="https://formsubmit.co/luanpanno@gmail.com" method="POST">
       <div className="wrapper">
         <p>Fique à vontade para me mandar um email!</p>
         <div className="fields">
-          <input type="text" placeholder="Insira seu nome" />
-          <input type="text" placeholder="Insira seu e-mail" />
-          <textarea placeholder="Insira sua mensagem" />
+          <input type="text" name="name" placeholder="Insira seu nome" />
+          <input type="text" name="email" placeholder="Insira seu e-mail" />
+          <textarea name="message" placeholder="Insira sua mensagem" />
         </div>
       </div>
       <button type="submit">Enviar</button>
