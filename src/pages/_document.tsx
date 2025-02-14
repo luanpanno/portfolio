94e5d9ddef92555593ae2704fc07c1d745ed1ddf
Luan Panno <luan.panno@lovelystay.com>
2025-02-14 16:38:22 -0300

    Add microsoft clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-rest-params */
 /* eslint-disable max-len */
 /* eslint-disable @next/next/no-page-custom-font */
 import Document, {
@@ -8,8 +9,11 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import Script from 'next/script';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+
+import { env } from '@utils/env';
 
 import i18nextConfig from '../../next-i18next.config';
 
@@ -63,6 +67,22 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {typeof window !== 'undefined' && (
+            <Script id="clarity-script" strategy="afterInteractive">
+              {`${(function (c, l, a, r, i, t, y) {
+                c[a] =
+                  c[a] ||
+                  function () {
+                    (c[a].q = c[a].q || []).push(arguments);
+                  };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = 'https://www.clarity.ms/tag/' + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+              })(window, document, 'clarity', 'script', env.clarityProjectId)}`}
+            </Script>
+          )}
         </body>
       </Html>
     );
---
+++ src/utils/env.ts
@@ -0,0 +1,3 @@
+export const env = {
+  clarityProjectId: process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID,
+};
