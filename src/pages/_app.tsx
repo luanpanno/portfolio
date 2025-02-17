9fb22fa4a78b177174a604a61dd63d735efd68d0
Luan Panno <luanpanno@gmail.com>
2025-02-17 08:23:37 -0300

    Change hotjar instalation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
 
 import { theme, GlobalStyles } from '@assets/styles';
+
+import { env } from '@utils/env';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <Component {...pageProps} />
     <GlobalStyles />
     <ToastContainer />
+    <Script id="hotjar-script" strategy="afterInteractive">
+      {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${env.hotjarProjectId},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+    </Script>
   </ThemeProvider>
 );
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,15 +9,10 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import Script from 'next/script';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
-import { env } from '@utils/env';
-
 import i18nextConfig from '../../next-i18next.config';
-
-declare const window: any;
 
 export default class MyDocument extends Document {
   static async getInitialProps(
@@ -65,48 +60,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
-          {typeof window !== 'undefined' && (
-            <Script id="hotjar-script">
-              {`${(function (h, o, t, j, a, r) {
-                h.hj =
-                  h.hj ||
-                  function () {
-                    (h.hj.q = h.hj.q || []).push(arguments);
-                  };
-                h._hjSettings = { hjid: env.hotjarProjectId, hjsv: 6 };
-                a = o.getElementsByTagName('head')[0];
-                r = o.createElement('script');
-                r.async = 1;
-                r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(
-                window,
-                document,
-                'https://static.hotjar.com/c/hotjar-',
-                '.js?sv='
-              )}`}
-            </Script>
-          )}
         </Head>
         <body>
           <Main />
           <NextScript />
-          {typeof window !== 'undefined' && (
-            <Script id="clarity-script" strategy="afterInteractive">
-              {`${(function (c, l, a, r, i, t, y) {
-                c[a] =
-                  c[a] ||
-                  function () {
-                    (c[a].q = c[a].q || []).push(arguments);
-                  };
-                t = l.createElement(r);
-                t.async = 1;
-                t.src = 'https://www.clarity.ms/tag/' + i;
-                y = l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t, y);
-              })(window, document, 'clarity', 'script', env.clarityProjectId)}`}
-            </Script>
-          )}
         </body>
       </Html>
     );
