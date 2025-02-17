d884e372442e485981413afadfd0382a15736187
Luan Panno <luanpanno@gmail.com>
2025-02-17 08:24:36 -0300

    Change clarity instalation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
@@ -23,6 +24,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+    </Script>
+    <Script id="clarity-script" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${env.clarityProjectId}");
         `}
     </Script>
   </ThemeProvider>
